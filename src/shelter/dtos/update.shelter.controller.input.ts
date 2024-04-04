31d8613c639cebb8cddf3aa1b3b45d2e24f52040
Einstein.dev <einstein.dev@hotmail.com>
2024-04-03 22:10:18 -0300

    feature: (Adicionando validação no Shelter input

--- src/shelter/dtos/update.shelter.controller.input.ts
+++ src/shelter/dtos/update.shelter.controller.input.ts
@@ -1,7 +1,20 @@
-export default class UpdateShelterDetailsUseCaseInput {
-	name: string
-	whatsapp: string
-	email: string
-	phone: string
+import { IsEmail, IsNotEmpty, IsNumber, IsString, Length } from "class-validator"
 
+export default class UpdateShelterDetailsUseCaseInput {
+	@IsString()
+	@IsNotEmpty()
+	name: string
+	@IsNotEmpty()
+	@IsString()
+	@Length(10,11 , { message: "errado"})
+	whatsapp: string
+	@IsNotEmpty()
+	@IsString()
+	@IsEmail()
+	email: string
+	@Length(10,11 , { message: "errado"})
+	@IsNotEmpty()
+	@IsString()
+	@IsNumber()
+	phone: string
 }
