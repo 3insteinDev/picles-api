3a2b98a280c871325b5ce940d6fc29fdafdce490
Einstein.dev <einstein.dev@hotmail.com>
2024-04-08 22:39:54 -0300

    feature: (criação da classe Pet)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { ShelterModule } from './shelter/shelter.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { config } from 'process';
+import { PetModule } from './pet/pet.module';
 
 @Module({
   imports: [
@@ -16,7 +17,8 @@ import { config } from 'process';
 		useFactory: async (config: ConfigService)=>({
 			uri: config.get<string>('DB_CONNECTION_STRING')
 		})
-	})
+	}),
+	PetModule
 	],
   controllers: [AppController],
   providers: [AppService],
